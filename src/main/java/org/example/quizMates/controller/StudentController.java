743408e33e13ac35ba4b80a7552b2a286129ee11
Wiggmund <mirracleourworld@gmail.com>
2023-08-01 17:28:29 +0300

    Implement retrieving students

--- src/main/java/org/example/quizMates/controller/StudentController.java
+++ src/main/java/org/example/quizMates/controller/StudentController.java
@@ -12,18 +12,21 @@ import org.example.quizMates.db.PostgreSQLConfig;
 import org.example.quizMates.dto.student.CreateStudentDto;
 import org.example.quizMates.exception.ExceptionResponse;
 import org.example.quizMates.exception.GlobalExceptionHandler;
+import org.example.quizMates.model.Student;
 import org.example.quizMates.repository.impl.StudentRepositoryImpl;
 import org.example.quizMates.service.StudentService;
 import org.example.quizMates.service.impl.StudentServiceImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/students")
 @RequiredArgsConstructor
 public class StudentController extends HttpServlet {
     private final StudentService studentService;
     private final static Gson gson = new Gson();
+    private final static String ID_REQ_PARAM = "id";
 
     public StudentController() {
         this(new StudentServiceImpl(
@@ -36,9 +39,18 @@ public class StudentController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         try {
-            writer.println("Your JSON response");
+            String requiredId = req.getParameter(ID_REQ_PARAM);
+
+            if (requiredId == null || requiredId.isEmpty()) {
+                List<Student> students = studentService.findAll();
+                writer.println(gson.toJson(students));
+            } else {
+                Student student = studentService.findById(Long.parseLong(requiredId));
+                writer.println(gson.toJson(student));
+            }
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            resp.setStatus(exceptionResponse.statusCode());
             writer.println(exceptionResponse.message());
         }
         writer.close();
@@ -53,6 +65,7 @@ public class StudentController extends HttpServlet {
             resp.setStatus(HttpServletResponse.SC_CREATED);
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            resp.setStatus(exceptionResponse.statusCode());
             writer.println(exceptionResponse.message());
         }
         writer.close();
@@ -65,6 +78,7 @@ public class StudentController extends HttpServlet {
             writer.println("Your JSON response");
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            resp.setStatus(exceptionResponse.statusCode());
             writer.println(exceptionResponse.message());
         }
         writer.close();
--- src/main/java/org/example/quizMates/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/org/example/quizMates/repository/impl/StudentRepositoryImpl.java
@@ -10,7 +10,9 @@ import org.example.quizMates.repository.StudentRepository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,17 +23,61 @@ public class StudentRepositoryImpl implements StudentRepository {
     private final static String ID_COL = "id";
     private final static String FIRST_NAME_COL = "first_name";
     private final static String LAST_NAME_COL = "last_name";
+    private final static String SELECT_BY_ID_SQL = String.format("SELECT * FROM %s WHERE %s = ?",
+            TABLE_NAME, ID_COL);
+    private final static String SELECT_ALL_SQL = String.format("SELECT * FROM %s", TABLE_NAME);
     private final static String CREATE_SQL = String.format("INSERT INTO %s(%s, %s) VALUES(?, ?)",
             TABLE_NAME, FIRST_NAME_COL, LAST_NAME_COL);
 
     @Override
-    public Optional<Student> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Student> findById(Long id) {
+        try(
+            Connection connection = dbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SELECT_BY_ID_SQL)
+        ) {
+            statement.setLong(1, id);
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    Student fetchedStudent = Student.builder()
+                            .id(resultSet.getLong(ID_COL))
+                            .firstName(resultSet.getString(FIRST_NAME_COL))
+                            .lastName(resultSet.getString(LAST_NAME_COL))
+                            .build();
+
+                    return Optional.of(fetchedStudent);
+                }
+
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new DBInternalException(e.getMessage());
+        }
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        try(
+            Connection connection = dbConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(SELECT_ALL_SQL);
+            ResultSet resultSet = statement.executeQuery()
+        ) {
+            List<Student> students = new ArrayList<>();
+
+            while(resultSet.next()) {
+                Student fetchedStudent = Student.builder()
+                        .id(resultSet.getLong(ID_COL))
+                        .firstName(resultSet.getString(FIRST_NAME_COL))
+                        .lastName(resultSet.getString(LAST_NAME_COL))
+                        .build();
+
+                students.add(fetchedStudent);
+            }
+
+            return students;
+        } catch (SQLException e) {
+            throw new DBInternalException(e.getMessage());
+        }
     }
 
     @Override
