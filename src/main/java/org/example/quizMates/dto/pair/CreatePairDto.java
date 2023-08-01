9a11b3f53e234e18fb1478b53c7b76a3573063b6
StefanSteuge <fotosteuge@gmail.com>
2023-08-01 22:43:17 +0300

    changed fields type

--- src/main/java/org/example/quizMates/dto/pair/CreatePairDto.java
+++ src/main/java/org/example/quizMates/dto/pair/CreatePairDto.java
@@ -10,6 +10,6 @@ import lombok.ToString;
 @Setter
 @ToString
 public class CreatePairDto {
-    private Long studentA;
-    private Long studentB;
+    private String studentA;
+    private String studentB;
 }
