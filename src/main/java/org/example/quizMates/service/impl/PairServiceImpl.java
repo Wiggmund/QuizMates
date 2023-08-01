88914ccb9374ae9d8a97dfcb1cec2652940cf2a1
StefanSteuge <fotosteuge@gmail.com>
2023-08-01 19:01:54 +0300

    added needed method realisations to the pairService

--- src/main/java/org/example/quizMates/service/impl/PairServiceImpl.java
+++ src/main/java/org/example/quizMates/service/impl/PairServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.quizMates.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.quizMates.dto.pair.CreatePairDto;
 import org.example.quizMates.dto.pair.UpdatePairDto;
+import org.example.quizMates.exception.ResourceNotFoundException;
 import org.example.quizMates.model.Pair;
 import org.example.quizMates.repository.PairRepository;
 import org.example.quizMates.service.PairService;
@@ -13,19 +14,23 @@ import java.util.List;
 public class PairServiceImpl implements PairService {
     private final PairRepository pairRepository;
 
+    private final static String PAIR_NOT_FOUND= "Pair with id %s not found";
+
     @Override
-    public Pair findById(Long aLong) {
-        return null;
+    public Pair findById(Long id) {
+        return pairRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(String.format(PAIR_NOT_FOUND)));
     }
 
     @Override
     public List<Pair> findAll() {
-        return null;
+        return pairRepository.findAll();
     }
 
     @Override
-    public void deleteById(Long aLong) {
-
+    public void deleteById(Long id) {
+        findById(id);
+        pairRepository.deleteById(id);
     }
 
     @Override
@@ -35,6 +40,7 @@ public class PairServiceImpl implements PairService {
 
     @Override
     public void updatePair(UpdatePairDto dto) {
-
+        findById(dto.getId());
+        pairRepository.updatePair(dto);
     }
 }
