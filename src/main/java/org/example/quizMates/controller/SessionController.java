3449a648ec5bb38deb4b96465cf07660bfff3f8f
Wiggmund <mirracleourworld@gmail.com>
2023-08-01 13:52:28 +0300

    Define main controllers

---
+++ src/main/java/org/example/quizMates/controller/SessionController.java
@@ -0,0 +1,63 @@
+package org.example.quizMates.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.quizMates.exception.ExceptionResponse;
+import org.example.quizMates.exception.GlobalExceptionHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/sessions")
+public class SessionController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.println("Your JSON response");
+        } catch (RuntimeException exception) {
+            ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            writer.println(exceptionResponse.message());
+        }
+        writer.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.println("Your JSON response");
+        } catch (RuntimeException exception) {
+            ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            writer.println(exceptionResponse.message());
+        }
+        writer.close();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.println("Your JSON response");
+        } catch (RuntimeException exception) {
+            ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            writer.println(exceptionResponse.message());
+        }
+        writer.close();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            writer.println("Your JSON response");
+        } catch (RuntimeException exception) {
+            ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
+            writer.println(exceptionResponse.message());
+        }
+        writer.close();
+    }
+}
