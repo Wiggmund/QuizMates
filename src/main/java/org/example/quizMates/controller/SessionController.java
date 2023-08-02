2438f183996953ab0e9df8c2b53aa63917b4bd04
Wiggmund <mirracleourworld@gmail.com>
2023-08-02 22:20:42 +0300

    Refactored SessionController

--- src/main/java/org/example/quizMates/controller/SessionController.java
+++ src/main/java/org/example/quizMates/controller/SessionController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.example.quizMates.config.ApplicationConfig;
 import org.example.quizMates.dto.session.CreateSessionDto;
 import org.example.quizMates.dto.session.UpdateSessionDto;
 import org.example.quizMates.exception.ExceptionResponse;
@@ -16,6 +17,7 @@ import org.example.quizMates.model.Session;
 import org.example.quizMates.service.LocalDateTimeAdapter;
 import org.example.quizMates.service.SessionService;
 import org.example.quizMates.service.impl.SessionServiceImpl;
+import org.example.quizMates.utils.ControllerHelper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -27,10 +29,7 @@ import java.util.List;
 public class SessionController extends HttpServlet {
     private final SessionService sessionService;
     private final static String ID_REQ_PARAM = "id";
-
-    private final static Gson gson = new GsonBuilder()
-            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-            .create();
+    private final static Gson gson = ApplicationConfig.GSON;
 
     public SessionController() {
         this(SessionServiceImpl.getInstance());
@@ -38,67 +37,54 @@ public class SessionController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
         try {
             String requiredId = req.getParameter(ID_REQ_PARAM);
             if (requiredId == null || requiredId.isEmpty()) {
                 List<Session> sessions = sessionService.findAll();
-                writer.println(gson.toJson(sessions));
+                ControllerHelper.writeResponse(resp, sessions, HttpServletResponse.SC_OK);
             } else {
                 Session session = sessionService.findById(Long.parseLong(requiredId));
-                writer.println(gson.toJson(session));
+                ControllerHelper.writeResponse(resp, session, HttpServletResponse.SC_OK);
             }
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
-            resp.setStatus(exceptionResponse.statusCode());
-            writer.println(exceptionResponse.message());
+            ControllerHelper.writeResponse(resp, exceptionResponse, exceptionResponse.statusCode());
         }
-        writer.close();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
         try {
             CreateSessionDto sessionDto = gson.fromJson(req.getReader(), CreateSessionDto.class);
             sessionService.createSession(sessionDto);
-            resp.setStatus(HttpServletResponse.SC_CREATED);
-            writer.println(sessionDto);
+            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
-            resp.setStatus(exceptionResponse.statusCode());
-            writer.println(exceptionResponse.message());
+            ControllerHelper.writeResponse(resp, exceptionResponse, exceptionResponse.statusCode());
         }
-        writer.close();
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
         try {
             UpdateSessionDto dto = gson.fromJson(req.getReader(), UpdateSessionDto.class);
             sessionService.updateSession(dto);
-            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
-            resp.setStatus(exceptionResponse.statusCode());
-            writer.println(exceptionResponse.message());
+            ControllerHelper.writeResponse(resp, exceptionResponse, exceptionResponse.statusCode());
         }
-        writer.close();
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
         try {
             long requiredId = Long.parseLong(req.getParameter(ID_REQ_PARAM));
             sessionService.deleteById(requiredId);
             resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
         } catch (RuntimeException exception) {
             ExceptionResponse exceptionResponse = GlobalExceptionHandler.handleException(exception);
-            resp.setStatus(exceptionResponse.statusCode());
-            writer.println(exceptionResponse.message());
+            ControllerHelper.writeResponse(resp, exceptionResponse, exceptionResponse.statusCode());
         }
-        writer.close();
     }
 }
