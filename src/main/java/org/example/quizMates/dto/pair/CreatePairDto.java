fd383a147ecc65d061923f17c2b9bd9954513ed4
Wiggmund <mirracleourworld@gmail.com>
2023-08-24 18:47:12 +0300

    Define equals and hashCode for Pair entity and dtos

--- src/main/java/org/example/quizMates/dto/pair/CreatePairDto.java
+++ src/main/java/org/example/quizMates/dto/pair/CreatePairDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Builder
 @Getter
 @Setter
@@ -12,4 +14,19 @@ import lombok.ToString;
 public class CreatePairDto {
     private Long studentA;
     private Long studentB;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreatePairDto that = (CreatePairDto) o;
+        return (Objects.equals(studentA, that.studentA) && Objects.equals(studentB, that.studentB))
+                ||
+                (Objects.equals(studentA, that.studentB) && Objects.equals(studentB, that.studentA));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentA, studentB);
+    }
 }
--- src/main/java/org/example/quizMates/dto/pair/UpdatePairDto.java
+++ src/main/java/org/example/quizMates/dto/pair/UpdatePairDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Builder
 @Getter
 @Setter
@@ -13,4 +15,20 @@ public class UpdatePairDto {
     private Long id;
     private Long studentA;
     private Long studentB;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UpdatePairDto pair = (UpdatePairDto) o;
+        return Objects.equals(id, pair.id) &&
+                (Objects.equals(studentA, pair.studentA) && Objects.equals(studentB, pair.studentB))
+                ||
+                (Objects.equals(studentA, pair.studentB) && Objects.equals(studentB, pair.studentA));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, studentA, studentB);
+    }
 }
